2a83220afe0fe2c409e2be481cb1d635f054d00b
Juan Diego <quintanajuanchy25@gmail.com>
2025-01-10 16:57:50 -0300

    add new function to edit and delete. Add new component into the forms

--- src/components/ProductDetail.tsx
+++ src/components/ProductDetail.tsx
@@ -1,10 +1,15 @@
-import { useNavigate } from "react-router-dom";
+import { ActionFunctionArgs, Form, useNavigate, redirect } from "react-router-dom";
 import { Product } from "../types";
 import { formatCurrency } from "../utils";
 
 type ProductDetailProps = {
   product: Product;
 };
+
+export async function action({params} : ActionFunctionArgs) {
+  
+  redirect('/')
+}
 
 export default function ProductDetail({ product }: ProductDetailProps) {
   const isAvailable = product.availability;
@@ -22,7 +27,20 @@ export default function ProductDetail({ product }: ProductDetailProps) {
       </td>
       <td className="p-3 text-lg text-gray-800 ">
         <div className="flex gap-2 items-center">
-            <button onClick={() => navigate(`/productos/${product.id}/editar`)} className="bg-indigo-600 text-white rounded-lg w-full p-2 uppercase font-bold text-xs">Editar</button>
+          <button
+            onClick={() => navigate(`/productos/${product.id}/editar`)}
+            className="bg-indigo-600 text-white rounded-lg w-full p-2 uppercase font-bold text-xs"
+          >
+            Editar
+          </button>
+
+          <Form className="w-full" method="POST" action={`productos/${product.id}/eliminar`}>
+            <input
+              type="submit"
+              value="Eliminar"
+              className="bg-red-600 text-white rounded-lg w-full p-2 uppercase font-bold text-xs"
+            />
+          </Form>
         </div>
       </td>
     </tr>
