8e83916e06cac4302af3ad09d074f4679643a458
Juan Diego <quintanajuanchy25@gmail.com>
2025-01-20 18:06:10 -0300

    add a patch logic for availability of product

--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -1,10 +1,4 @@
-import {
-  safeParse,
-  number,
-  pipe,
-  transform,
-  parse,
-} from "valibot";
+import { safeParse, number, pipe, transform, parse } from "valibot";
 import axios from "axios";
 import {
   DraftProductSchema,
@@ -81,7 +75,7 @@ export async function updateProducts(data: ProductData, id: Product["id"]) {
       availability: toBoolean(data.availability.toString()),
     });
 
-    if(result.success) {
+    if (result.success) {
       const url = `${import.meta.env.VITE_API_URL}/api/products/${id}`;
       await axios.put(url, result.output);
     }
@@ -90,11 +84,20 @@ export async function updateProducts(data: ProductData, id: Product["id"]) {
   }
 }
 
+export async function updateProductAvailability(id: Product["id"]) {
+  try {
+    const url = `${import.meta.env.VITE_API_URL}/api/products/${id}`;
+    await axios.patch(url);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 export async function deleteProduct(id: Product["id"]) {
   try {
     const url = `${import.meta.env.VITE_API_URL}/api/products/${id}`;
-    await axios.delete(url)
+    await axios.delete(url);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
